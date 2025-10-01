19175c070c0773d95534564c9ddfa6f1e23c001a
Ekre Ceannmor <126588178+eceannmor@users.noreply.github.com>
2025-10-01 21:25:14 +0200

    tutorial 1 sample code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-## TRSensorsMod
-See the installation and examples in at [TRSensorsExample.c](./TRSensorsExample.c)
+# ISS resources
+This repo contains everything for passing the classes, as well as much more detailed explanations of concepts that I could not recall of the topp of my head / didn't have time to go over. 
+
+If something is crossed out, it will be added in some time, when/if it becomes relevant.
+
+## Table of contents
+### Project templates
+I encourage you to use these as a starting point for the projects.
+* [p1_communication_protocol.ino]()
+* [p2_line_follower.ino]()
+* ~~p3_ball_balancer.ino~~
+
+More samples will be added when I confirm that we will actually be doing those projects
+
+### General examples
+* [TRSensorsExample.ino](./TRSensorsExample.ino) - front bumper sensors for the mobile robot
+* [motors.ino]() - Communicating with the motors
+* ~~sonar.ino~~
+
+### Everything else
+Code written in class, and other miscialenious code is in the [misc/](./misc/) directory.<br/>
+[printf_fix.ino]() - code to fix the `printf` function. __*Not needed for the classes*__. Please seriously, this is for your convenience only.
---
+++ misc/tutorial_001.ino
@@ -0,0 +1,64 @@
+// this defines "ENCODER_PIN_R/L" as a preprocessor macro the preprocessor runs
+// before the compiler and replaces all occurrences of "ENCODER_PIN_R/L"
+// with 2/3
+#define ENCODER_PIN_R 2
+// for anyone wanting to write "proper c++", replace the directive with a
+// constexpr variable
+constexpr int ENCODER_PIN_L = 3;
+
+// this can be dedclared both out of scope of any function (can be read from
+// anywhere), or in the scope of the main loop (only available to the loop)
+char readByte;
+
+// https://en.cppreference.com/w/cpp/types/integer.html
+uint16_t counter_r; // arduino supports C standard library types
+uint16_t counter_l;
+
+void inc_r() {
+    counter_r++;
+}
+
+void inc_l() {
+    counter_l++;
+}
+
+void setup() {
+    // Start serial communication at a rate of 9600 baud
+    Serial.begin(9600);
+
+    // attachInterrupt() requires a ""void(*func)()"" ?
+    // This is a pointer to a "void ..()" function, like inc_r above
+    // How to read complicated types:
+    // https://cseweb.ucsd.edu/~ricko/rt_lt.rule.html
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_R), inc_r, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_L), inc_l, RISING);
+}
+
+void printCounters() {
+    // because we declared the counters at the global score, we can access the 
+    // values from anywhere
+    Serial.print("Counter values: Left=");
+    Serial.print(counter_l);
+    Serial.print(" Right=");
+    Serial.print(counter_l);
+    Serial.println();
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        // first byte (character) of the command
+        readByte = Serial.read();
+
+        if ('m' == readByte) {
+            // skip one byte (space character)
+            Serial.read();
+            int value = Serial.parseInt();
+            // execute Move command here
+        } else if ('s' == readByte) {
+            /*  no need to read any more data
+                code to stop all motors goes here */
+        } /*else if (... all other commands ...) {
+            ... 
+        }*/
+    }
+}
