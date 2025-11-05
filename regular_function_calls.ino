c1fe7826805506e01e1c5bd50800a877b8468d0a
Ekre Ceannmor <126588178+eceannmor@users.noreply.github.com>
2025-11-05 07:59:57 +0100

    updated timers example

--- regular_function_calls.ino
+++ regular_function_calls.ino
@@ -1,32 +1,24 @@
 // This is a short example on how to make a function that executes every N seconds without stopping everything else.
 
-#include <time.h>
-
-static constexpr int TEN_SECONDS{10};
-static constexpr int TWENTY_SECONDS{20};
-
 void setup() {}
 
-static time_t ten_second_timer = time(NULL);
-static time_t twenty_second_timer = time(NULL);
+static time_t timer_100ms = millis();
+static time_t timer_1s = millis();
 
 void loop() {
-    const time_t current_time = time(NULL);
+    const time_t current_time = millis();
 
-    if (current_time - ten_second_timer >= TEN_SECONDS) {
-        ten_second_timer = current_time;
-        // this will run every 10 seconds
-        do_something_rarely();
+    if (current_time - timer_100ms >= 100) { // timer for 100 ms
+        timer_100ms = current_time;
+        do_something();
     }
 
-    if (current_time - twenty_second_timer >= TWENTY_SECONDS) {
-        twenty_second_timer = current_time;
-        // this will run every 20 seconds
+    if (current_time - timer_1s >= 1000) { // timer for 1 second
+        timer_1s = current_time;
         do_something_else();
     }
 
-    // this will run constantly, never waiting for the 10 and 20 seconds timers to fire
+    // this will run constantly, never waiting for timers to fire
     do_something_constantly();
 }
-
-// Use std::chrono if you prefer, this is just an example
+// for higher precision use either micros() or std::chrono::steady_clock
